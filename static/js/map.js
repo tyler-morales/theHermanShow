1c91ae9adb933816f4340ded043dfd09a72789e4
Tyler Morales <moratyle@gmail.com>
2020-10-04 16:22:11 -0600

    🗃 Reset data in Hugo

--- static/js/map.js
+++ static/js/map.js
@@ -1,49 +1,61 @@
 function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
-    center: { lat: -28.024, lng: 140.887 },
-  });
-  // Create an array of alphabetical characters used to label the markers.
-  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  // Add some markers to the map.
-  // Note: The code uses the JavaScript Array.prototype.map() method to
-  // create an array of markers based on a given "locations" array.
-  // The map() method here has nothing to do with the Google Maps API.
-  const markers = locations.map((location, i) => {
-    return new google.maps.Marker({
-      position: location,
-      label: labels[i % labels.length],
-    });
-  });
-  // Add a marker clusterer to manage the markers.
+  let map;
+  let bounds = new google.maps.LatLngBounds();
+  let mapOptions = {
+    mapTypeId: 'roadmap'
+  };
 
-  new MarkerClusterer(map, markers, {
-    imagePath:
-      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  // Display a map on the web page
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  map.setTilt(50);
+
+  // Multiple markers location, latitude, and longitude
+  let markers = [
+    ['Brooklyn Museum, NY', 40.671531, -73.963588],
+  ];
+
+  // Info window content
+  let infoWindowContent = [
+    [
+      '<div class="info_content">' +
+      `<h3>name</h3>` +
+      '<p>The Brooklyn Museum is an art museum located in the New York City borough of Brooklyn.</p>' +
+      '</div>'
+    ]
+  ];
+
+  // Add multiple markers to map
+  let infoWindow = new google.maps.InfoWindow(),
+    marker, i;
+
+  // Place each marker on the map  
+  for (i = 0; i < markers.length; i++) {
+    let position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+    bounds.extend(position);
+    marker = new google.maps.Marker({
+      position: position,
+      map: map,
+      title: markers[i][0]
+    });
+
+    // Add info window to marker    
+    google.maps.event.addListener(marker, 'click', (function (marker, i) {
+      return function () {
+        infoWindow.setContent(infoWindowContent[i][0]);
+        infoWindow.open(map, marker);
+      }
+    })(marker, i));
+
+    // Center the map to fit all markers on the screen
+    map.fitBounds(bounds);
+  }
+
+  // Set zoom level
+  let boundsListener = google.maps.event.addListener((map), 'bounds_changed', function (event) {
+    this.setZoom(5);
+    google.maps.event.removeListener(boundsListener);
   });
+
 }
-const locations = [
-  { lat: -31.56391, lng: 147.154312 },
-  { lat: -33.718234, lng: 150.363181 },
-  { lat: -33.727111, lng: 150.371124 },
-  { lat: -33.848588, lng: 151.209834 },
-  { lat: -33.851702, lng: 151.216968 },
-  { lat: -34.671264, lng: 150.863657 },
-  { lat: -35.304724, lng: 148.662905 },
-  { lat: -36.817685, lng: 175.699196 },
-  { lat: -36.828611, lng: 175.790222 },
-  { lat: -37.75, lng: 145.116667 },
-  { lat: -37.759859, lng: 145.128708 },
-  { lat: -37.765015, lng: 145.133858 },
-  { lat: -37.770104, lng: 145.143299 },
-  { lat: -37.7737, lng: 145.145187 },
-  { lat: -37.774785, lng: 145.137978 },
-  { lat: -37.819616, lng: 144.968119 },
-  { lat: -38.330766, lng: 144.695692 },
-  { lat: -39.927193, lng: 175.053218 },
-  { lat: -41.330162, lng: 174.865694 },
-  { lat: -42.734358, lng: 147.439506 },
-  { lat: -42.734358, lng: 147.501315 },
-  { lat: -42.735258, lng: 147.438 },
-  { lat: -43.999792, lng: 170.463352 },
-];
+// Load initialize function
+google.maps.event.addDomListener(window, 'load', initMap);
